8805f02c1b458335174df1d0a40b437bee24970e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2026-01-10 14:26:34 +0100

    Update Tour.java

--- HeuristicApproach/Tour.java
+++ HeuristicApproach/Tour.java
@@ -185,8 +185,8 @@ public final class Tour implements Comparable<Tour> {
                 }
             }
         double probability = max / (double) data.StopsCount;
-        boolean again = Math.random() < probability;
-        if ((!again && improvementCounter > 0) || (again && improvementCounter < max && this.StagnationBreaker(data))) {
+        boolean again = Math.random() > probability;
+        if ((again && improvementCounter > 0) || (!again && improvementCounter < max && this.StagnationBreaker(data))) {
             int n = (int) (Math.random() * Math.sqrt(data.StopsCount));
             Move move = new Move(0, this.Sequence.length - 1);
             for (int i = 0; i < n; i++)
